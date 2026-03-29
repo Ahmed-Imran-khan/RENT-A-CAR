19b46a2fd7b271681b43b3a6d3a1e77c76c7a840
Ahmed Imran <ahmedimrankhaan@gmail.com>
2026-03-29 14:11:08 +0500

    updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,7 +36,7 @@ function App() {
           />
           <Route
             path="/vehicles"
-            element={
+            element={ 
               <PrivateRoute>
                 <Vehicles />
               </PrivateRoute>
